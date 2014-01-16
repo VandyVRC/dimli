83fd164a913f4d32ca1b1c61784f5daf5498c932
Matt Isner <matthew.isner@deque.com>
2014-01-16 18:51:19 -0500

    Tabs to spaces, and minor formatting

--- _javascript/docready.js
+++ _javascript/docready.js
@@ -1,203 +1,156 @@
+// jshint jquery: true
+
 $(document).ready(function () {
 
-	// HOVER LANTERN NAV ITEM
+    // Hover Lantern nav item
+  $('li#nav_lantern')
+    .hover(
+      function (event) {
+        event.stopPropagation();
+        $(this).css({ cursor: 'pointer' });
+      }, function (event) {
+        event.stopPropagation();
+        $(this).css({ cursor: 'default' });
+      })
+    .children().hover(
+      function () {
+        return false;
+      });
 
-	$('li#nav_lantern').hover(
-		function(event)
-		{
-			event.stopPropagation();
-			$(this).css({ cursor: 'pointer' });
-		},
-		function(event)
-		{
-			event.stopPropagation();
-			$(this).css({ cursor: 'default' });
-		})
-	.children().hover(
-		function()
-		{
-			return false;
-		});
+  
+    // Hover dropdown list item
+  $('div.nav_dropdown_item')
+    .hover(
+      function () {
+        $(this).css({ backgroundColor: '#444466' });
+        return false;
+      }, function () {
+        $(this).css({ backgroundColor: '#669' });
+        return false;
+      });
 
-	
-	// HOVER DROPDOWN LIST ITEM
-
-	$('div.nav_dropdown_item').hover(
-		function(event)
-		{
-			$(this).css({ backgroundColor: '#444466' });
-			return false;
-		}, function()
-		{
-			$(this).css({ backgroundColor: '#669' });
-			return false;
-		});
-
-	
-	// CLICK NAV ITEM
-
-	$('li.nav_item').click(showThisDropdown).children().click(
-		function(event)
-		{
-			return false;
-			// Prevent children from triggering the event
-		});
+  
+    // Click nav item
+  $('li.nav_item').click(showThisDropdown).children()
+    .click(
+      function () {
+        return false; // Prevent children from triggering the event
+      });
 
 
-	// CLICK LANTERN NAV ITEM
+    // Click Lantern nav item
+  $('li#nav_lantern').bind('click.focus', function (event) {
+    event.stopPropagation();
 
-	$('li#nav_lantern').bind('click.focus', 
-		function(event)
-		{
-			event.stopPropagation();
+    $('div.nav_dropdown').hide();
 
-			$('div.nav_dropdown').hide();
+    var msgs = ['aim for', 'be after', 'beat the bushes', 'bird-dog',
+      'bob for', 'cast about', 'chase', 'comb', 'delve', 'delve for',
+      'dig for', 'dragnet', 'explore', 'fan', 'ferret out', 'fish',
+      'fish for', 'follow', 'go after', 'gun for', 'hunt', 'inquire',
+      'investigate', 'leave no stone unturned', 'look about',
+      'look around', 'look high and low', 'mouse', 'nose', 'prowl',
+      'pursue', 'quest', 'ransack', 'root', 'run after', 'scout',
+      'scratch', 'search for', 'search out', 'seek', 'sniff out',
+      'track down'];
+      
+      var msg = msgs[Math.floor(Math.random() * msgs.length)];
 
-			var msgs = [
-				'aim for',
-				'be after',
-				'beat the bushes',
-				'bird-dog','bob for',
-				'cast about',
-				'chase',
-				'comb',
-				'delve',
-				'delve for',
-				'dig for',
-				'dragnet',
-				'explore',
-				'fan',
-				'ferret out',
-				'fish',
-				'fish for',
-				'follow',
-				'go after',
-				'gun for',
-				'hunt',
-				'inquire',
-				'investigate',
-				'leave no stone unturned',
-				'look about',
-				'look around',
-				'look high and low',
-				'mouse',
-				'nose',
-				'prowl',
-				'pursue',
-				'quest',
-				'ransack',
-				'root',
-				'run after',
-				'scout',
-				'scratch',
-				'search for',
-				'search out',
-				'seek',
-				'sniff out',
-				'track down'
-			];
-			
-			var msg = msgs[Math.floor(Math.random() * msgs.length)];
+        // Show random synonym as input placeholder
+      $('input#lantern_search').attr('placeholder', msg);
 
-			$('input#lantern_search').attr('placeholder', msg);
-			// Show random synonym as input placeholder
+      $(this).find('div.nav_dropdown').show();
 
-			$(this).find('div.nav_dropdown').show();
+      $('input#lantern_search').select().focus();
 
-			$('input#lantern_search').select().focus();
+      closeAllNavLists_prep();
 
-			closeAllNavLists_prep();
+    });
 
-		});
+  $('li#nav_lantern div.nav_dropdown').click(
+    function (event) {
+      event.stopPropagation();
+    });
 
-	$('li#nav_lantern div.nav_dropdown').click(
-		function(event)
-		{
-			event.stopPropagation();
-		});
-
-	$('input#lantern_gettyToggle').click(
-		function()
-		{
-			$('input#lantern_search').focus();
-		});
+  $('input#lantern_gettyToggle').click(
+    function () {
+      $('input#lantern_search').focus();
+    });
 
 
-	// SUBMIT LANTERN SEARCH
+    // Submit Lantern search
+  function lanternSearch_submit()
+  {
+    var text = $('input#lantern_search').val();
+    var gToggle = $('input#lantern_gettyToggle').prop('checked');
+    console.log('lantern-search submitted');
+    console.log('lantern-search string -- "'+text+'"');
+    console.log('lantern-search authority toggle -- '+gToggle);
+    lantern_search(text, gToggle, 1);
+  }
 
-	function lanternSearch_submit(event)
-	{
-		var text = $('input#lantern_search').val();
-		var gToggle = $('input#lantern_gettyToggle').prop('checked');
-		console.log('lantern-search submitted');
-		console.log('lantern-search string -- "'+text+'"');
-		console.log('lantern-search authority toggle -- '+gToggle);
-		lantern_search(text, gToggle, 1);
-	}
+  $('button#lantern_submit').click(lanternSearch_submit);
+  $('input#lantern_search').keypress(
+    function (event) {
+      var enterWhich = event.which && event.which == 13;
+      var enterKeycode = event.keyCode && event.keyCode == 13;
 
-	$('button#lantern_submit').click(lanternSearch_submit);
-	$('input#lantern_search').keypress(
-		function(event)
-		{
-			if (
-				(event.which && event.which == 13) || 
-				(event.keyCode && event.keyCode == 13)
-				)
-			{
-				lanternSearch_submit();
-			}
-		});
-			
+      if (enterWhich || enterKeycode) {
+        lanternSearch_submit();
+      }
+
+    });
+      
 
 
-	//---------------------------------------------
-	//	Define events for navigation menu buttons
-	//---------------------------------------------
+  //---------------------------------------------
+  //  Define events for navigation menu buttons
+  //---------------------------------------------
 
-	$('div#nav_browseUsers').click(usersBrowse_load);
+  $('div#nav_browseUsers').click(usersBrowse_load);
 
-	$('div#nav_registerUser').click(registerNewUser_load);
+  $('div#nav_registerUser').click(registerNewUser_load);
 
-	$('div#nav_export').click(export_load);
+  $('div#nav_export').click(export_load);
 
-	$('div#nav_findOrder').click(function() { 
-		findOrders_loadForm(1, 'date_needed', 'ASC');
-	});
+  $('div#nav_findOrder').click(function () {
+    findOrders_loadForm(1, 'date_needed', 'ASC');
+  });
 
-	$('div#nav_createOrder').click(function() { 
-		createOrder_load_form(); 
-	});
+  $('div#nav_createOrder').click(createOrder_load_form);
 
-	$('div#nav_createWork').click(createBuiltWork);
+  $('div#nav_createWork').click(createBuiltWork);
 
-	$('div#nav_createRepository').click(createRepository);
+  $('div#nav_createRepository').click(createRepository);
 
-	$('div#nav_viewOrphanedWorks').click(view_orphaned_works);
+  $('div#nav_viewOrphanedWorks').click(view_orphaned_works);
 
-	$('div#nav_viewOrphanedImages').click(view_orphaned_images);
+  $('div#nav_viewOrphanedImages').click(view_orphaned_images);
 
-	$('div#nav_import').click(function() {
-		window.location = "import.php";
-	});
+  $('div#nav_import').click(function() {
+    window.location = 'import.php';
+  });
 
-	//----------------------------
-	//	Legacy Reader Navigation
-	//----------------------------
+  //----------------------------
+  //  Legacy Reader Navigation
+  //----------------------------
 
-	$(function() {
-		$('form[id=legacyReader-form] input').keypress(function(e) {
-			if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13))
-			{
-				$(this).parents('form')
-					.find('input[id=legacyReader-jumpToRecord]')
-					.click();
+  $(function () {
+    $('form[id=legacyReader-form] input').keypress(function (event) {
+      var enterWhich = event.which && event.which == 13;
+      var enterKeycode = event.keyCode && event.keyCode == 13;
 
-				return false;
-			}
-			else
-			{
-				return true;
-			}
-		});
-	});
+      if (enterWhich || enterKeycode) {
+        $(this).parents('form')
+          .find('input[id=legacyReader-jumpToRecord]')
+          .click();
+        return false;
+
+      } else {
+        return true;
+
+      }
+    });
+  });
+  
 });
--- _javascript/ui_feedback.js
+++ _javascript/ui_feedback.js
@@ -1,43 +1,38 @@
-function msg(msg_arr, type)
-{
-	var msg_str = '';
+// jshint jquery: true
 
-	// Use the msg_arr parameter to build the message string and console message
-	$.each(msg_arr, function(index, value) {
-		console.info('user message: "'+value+'"');
-		msg_str += value+'<br>';
-	});
+function msg(msg_arr, type) {
+  var msg_str = '';
 
-	// Give the message its style and content
-	$('div#message_text').attr('class','').addClass(type).html(msg_str);
+    // Use the msg_arr parameter to build the message string and console message
+  $.each(msg_arr, function (index, value) {
+    console.info('user message: "' + value + '"');
+    msg_str += value + '<br>';
+  });
 
-	// Show the message, and bind an event to hide it if the user clicks it
-	$('div#message_wrapper').fadeIn(400).click(function() {
-		$(this).hide();
-	});
+    // Give the message its style and content
+  $('div#message_text').attr('class','').addClass(type).html(msg_str);
 
-	// Bind handle to hide message after a delay
-	var msgTimeoutHandle = window.setTimeout(function() {
-		$('div#message_wrapper').fadeOut(400);
-		window.clearTimeout(msgTimeoutHandle);
-	}, 3200);
+    // Show the message, and bind an event to hide it if the user clicks it
+  $('div#message_wrapper').fadeIn(400).click(function () {
+    $(this).hide();
+  });
+
+    // Bind handle to hide message after a delay
+  var msgTimeoutHandle = window.setTimeout(function () {
+    $('div#message_wrapper').fadeOut(400);
+    window.clearTimeout(msgTimeoutHandle);
+  }, 3200);
 }
 
-function input_error(input)
-{
-	var defaultBorder = $(input).css('borderColor');
-	var defaultBg = $(input).css('backgroundColor');
+function input_error(input) {
+  $(input).addClass('input_error');
 
-	$(input).addClass('input_error');
+  $(input).focus(function () {
+    reset_field($(input));
+  });
 
-	function reset_field(input)
-	{
-		$(input).removeClass('input_error');
-		$(input).unbind('focus', reset_field);
-	}
-
-	$(input).focus(function()
-	{
-		reset_field($(input));
-	});
+  function reset_field(input) {
+    $(input).removeClass('input_error');
+    $(input).unbind('focus', reset_field);
+  }
 }
