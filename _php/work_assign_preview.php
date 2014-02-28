cf2028e04a040a8c2a1a89dc494ca25da62c6710
Cristy Danford <cristydanford@gmail.com>
2014-02-27 18:12:29 -0600

    Oops. Missed one

--- _php/work_assign_preview.php
+++ _php/work_assign_preview.php
@@ -34,7 +34,7 @@ $result = db_query($mysqli, $sql);
 <div class="workRecord_thumb defaultCursor"
 	style="position: absolute; top: 0; right: 0;">
 
-	<img src="<?php echo $webroot.'/'.$image_src; ?>thumb/<?php echo $prefLegId; ?>.jpg"
+	<img src="<?php echo $image_dir; ?>thumb/<?php echo $prefLegId; ?>.jpg"
 	onclick="image_viewer('<?php echo $prefLegId; ?>');">
 
 </div>
